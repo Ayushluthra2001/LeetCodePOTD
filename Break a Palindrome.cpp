c7218e4a6e1826f145bb68cc69cfb07617df3e20
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-10-10 17:34:56 +0530

    Create Break a Palindrome.cpp

---
+++ Break a Palindrome.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+   
+    string breakPalindrome(string palindrome) {
+        if(palindrome.length()<2) return "";
+        for(int i=0;i<palindrome.length()/2;i++){
+            if(palindrome[i]!='a'){
+                palindrome[i]='a';
+                return palindrome;
+            } 
+        }
+        palindrome[palindrome.length()-1]='b';
+        return palindrome;
+    }
+};
