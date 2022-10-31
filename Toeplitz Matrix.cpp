0e40842b440bf3c3bf5b6291bf3545c306e17e61
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-10-31 16:59:09 +0530

    Create Toeplitz Matrix.cpp

---
+++ Toeplitz Matrix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+     int n=matrix.size();
+        int m= matrix[0].size();
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(i-1>=0 && j-1>=0){
+                    if(matrix[i][j]!=matrix[i-1][j-1]) return false;
+                }
+            }
+        }
+        return true;
+    }
+};
