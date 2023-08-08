58284da0741172afb307fa99d305ca52681922e3
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-08-08 18:19:51 +0530

    Create Search in Rotated Sorted Array.cpp

---
+++ Search in Rotated Sorted Array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        for(int i=0;i<nums.size();i++){
+            if(target==nums[i]) return i;
+        }
+        return -1;
+    }
+};
