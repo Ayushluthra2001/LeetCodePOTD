e623dedf372d401099300ebe548c9da735fd2d8d
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-09-03 14:12:21 +0530

    Create Unique Paths(Recursion Dp Top down approach).cpp

---
+++ Unique Paths(Recursion Dp Top down approach).cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int solve(int m ,int n , int i,int j,vector<vector<int>>&dp){
+        if(i>=m || j>=n) return 0;
+        if(i==m-1 && j==n-1) return 1;
+        if(dp[i][j]!=-1 ) return dp[i][j];
+        return dp[i][j]=solve(m,n,i+1,j,dp)+solve(m,n,i,j+1,dp);
+    }
+    int uniquePaths(int m, int n) {
+        
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return solve(m,n,0,0,dp);
+
+    }
+};
