f39002997fdf8a02fef4ac012091642237aec8e1
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-01-16 23:06:25 +0530

    Create Insert Delete GetRandom O(1)C.cpp

---
+++ Insert Delete GetRandom O(1)C.cpp
@@ -0,0 +1,37 @@
+class RandomizedSet {
+public:
+    vector<int>v;
+    unordered_map<int,int>mapping;
+    RandomizedSet() {
+       
+    }
+    
+    bool insert(int val) {
+        if(mapping.find(val)!=mapping.end()) return false;
+        v.push_back(val);
+        mapping[val]=v.size()-1;
+        return true;
+    }
+    
+    bool remove(int val) {
+        if(mapping.find(val)==mapping.end()) return false;
+        auto it =mapping.find(val);
+        v[it->second]=v.back();
+        v.pop_back();
+        mapping[v[it->second]]=it->second;
+        mapping.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        return v[rand()%v.size()];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
