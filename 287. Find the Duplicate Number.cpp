cbf41bc8394bf59614d6f6cc2afea4c5fd79e956
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-03-24 17:35:15 +0530

    Create 287. Find the Duplicate Number.cpp

---
+++ 287. Find the Duplicate Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+     unordered_map<int,int>mapping;
+     for(auto i : nums){
+        mapping[i]++;
+     }   
+     for(auto i : mapping){
+        if(i.second>=2) return i.first;
+     }
+     return -1;
+    }
+};
