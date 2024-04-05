ec93ca038f9c92f15aeda88cf801a3358cb5ae2f
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-04-05 18:13:53 +0530

    Create 1544. Make The String Great.cpp

---
+++ 1544. Make The String Great.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        if(s.length()==1) return s;
+        stack<char>st;
+        string ans="";
+        st.push(s[0]);
+        int i=1;
+        while(i<s.length()){
+           if(!st.empty()){
+                char top=st.top();
+                char currChar=s[i];
+                if(currChar==tolower(currChar)){
+                    if(toupper(currChar)==top) st.pop();
+                    else st.push(s[i]);
+                }else{
+                    if(tolower(currChar)==top) st.pop();
+                    else st.push(s[i]);
+                }
+           }else st.push(s[i]);
+           i++;
+        }
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
